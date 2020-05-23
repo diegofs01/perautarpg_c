9b3822034ada77a62a62fc7903293b2ea50399f4
Tode <emerson_abedias@hotmail.com>
2020-05-23 13:59:45 -0300

    Quebra em funcoes
    alguma smelhorias de codigo

--- perautarpg.c
+++ perautarpg.c
@@ -16,87 +16,77 @@
 #define MIN_Y_LIMITATION 24
 #define SCROLL_MOVE 8
 
+int spriteX = 16, spriteY = 24;
+int mapX = 152, mapY = 144;
+int i = 0, spriteBackupX = 0, spriteBackupY = 0;
+	
+UBYTE menuPos = 0;
+UBYTE currentKey;
+
+void init();
+void captureInput();
+
 void main() {
 	
-	int spriteX = 16, spriteY = 24;
-	int mapX = 152, mapY = 144;
-	int i = 0, spriteBackupX = 0, spriteBackupY = 0;
-	
-	UBYTE menuPos = 0;
-	UBYTE currentKey;
-	
-	DISPLAY_OFF;
-	
-	set_bkg_palette(0, 8, map1Palette);
-	set_bkg_data(0, tileset1Len, tileset1);
-	set_bkg_tiles(0, 0, map1Width, map1Height, map1);	
-	
-	while(i < (windowWidth * windowHeight)) {
-		window[i] += tileset1Len;
-		i++;
-	}
-	set_win_data(tileset1Len, borderLen, border);
-	set_win_tiles(0, 0, windowWidth, windowHeight, window);
-	
-	SPRITES_8x8;
-	set_sprite_palette(0, 1, spritePalette);
-	set_sprite_data(0, tileset1Len, tileset1);
-	set_sprite_tile(0, 14);
-	move_sprite(0, spriteX, spriteY);
-	
-	SHOW_BKG;
-	SHOW_SPRITES;
-	DISPLAY_ON;
+	init();
 	
 	while(1) {
+		captureInput();
+		delay(100);
+		wait_vbl_done();
+	}
+}
+
+void captureInput() {
+			
+	currentKey = joypad();
 		
-		currentKey = joypad();
-		
-		if (currentKey & J_RIGHT) {
-			if (spriteX < MAX_X_LIMITATION) {
-				spriteX += SCROLL_MOVE;
-				move_sprite(0, spriteX, spriteY);
-			} else {
-				if (mapX < ((map1Width - 1) * 8)) {
-					mapX += SCROLL_MOVE;
-					scroll_bkg(SCROLL_MOVE, 0);
-				}
+	if (currentKey & J_RIGHT) {
+		if (spriteX < MAX_X_LIMITATION) {
+			spriteX += SCROLL_MOVE;
+			move_sprite(0, spriteX, spriteY);
+		} else {
+			if (mapX < ((map1Width - 1) * 8)) {
+				mapX += SCROLL_MOVE;
+				scroll_bkg(SCROLL_MOVE, 0);
 			}
 		}
-		if (currentKey & J_LEFT) {
-			if(spriteX > MIN_X_LIMITATION) {
-				spriteX -= SCROLL_MOVE;
-				move_sprite(0, spriteX, spriteY);
-			} else {
-				if (mapX > 160) {
-					mapX -= SCROLL_MOVE;
-					scroll_bkg(-SCROLL_MOVE, 0);
-				}
+	}
+	if (currentKey & J_LEFT) {
+		if(spriteX > MIN_X_LIMITATION) {
+			spriteX -= SCROLL_MOVE;
+			move_sprite(0, spriteX, spriteY);
+		} else {
+			if (mapX > 160) {
+				mapX -= SCROLL_MOVE;
+				scroll_bkg(-SCROLL_MOVE, 0);
 			}
 		}
-		if (currentKey & J_UP) {
-			if(spriteY > MIN_Y_LIMITATION) {
-				spriteY -= SCROLL_MOVE;
-				move_sprite(0, spriteX, spriteY);
-			} else {
-				if(mapY > 152) {
-					mapY -= SCROLL_MOVE;
-					scroll_bkg(0, -SCROLL_MOVE);
-				}
+	}
+	if (currentKey & J_UP) {
+		if(spriteY > MIN_Y_LIMITATION) {
+			spriteY -= SCROLL_MOVE;
+			move_sprite(0, spriteX, spriteY);
+		} else {
+			if(mapY > 152) {
+				mapY -= SCROLL_MOVE;
+				scroll_bkg(0, -SCROLL_MOVE);
 			}
 		}
-		if (currentKey & J_DOWN) {
-			if(spriteY < MAX_Y_LIMITATION) {
-				spriteY += SCROLL_MOVE;
-				move_sprite(0, spriteX, spriteY);
-			} else {
-				if(mapY < (map1Height * SCROLL_MOVE)) {
-					mapY += SCROLL_MOVE;
-					scroll_bkg(0, SCROLL_MOVE);
-				}
+	}
+	if (currentKey & J_DOWN) {
+		if(spriteY < MAX_Y_LIMITATION) {
+			spriteY += SCROLL_MOVE;
+			move_sprite(0, spriteX, spriteY);
+		} else {
+			if(mapY < (map1Height * SCROLL_MOVE)) {
+				mapY += SCROLL_MOVE;
+				scroll_bkg(0, SCROLL_MOVE);
 			}
 		}
-		if (currentKey & J_START) {
+	}
+	
+	if (currentKey & J_START) {
 			
 			spriteBackupX = spriteX;
 			spriteBackupY = spriteY;
@@ -186,7 +176,29 @@ void main() {
 			}
 
 		}
-		delay(150);
-	}
+}
 
+void init() {
+	DISPLAY_OFF;
+	
+	set_bkg_palette(0, 8, map1Palette);
+	set_bkg_data(0, tileset1Len, tileset1);
+	set_bkg_tiles(0, 0, map1Width, map1Height, map1);	
+	
+	while(i < (windowWidth * windowHeight)) {
+		window[i] += tileset1Len;
+		i++;
+	}
+	set_win_data(tileset1Len, borderLen, border);
+	set_win_tiles(0, 0, windowWidth, windowHeight, window);
+	
+	SPRITES_8x8;
+	set_sprite_palette(0, 1, spritePalette);
+	set_sprite_data(0, tileset1Len, tileset1);
+	set_sprite_tile(0, 14);
+	move_sprite(0, spriteX, spriteY);
+	
+	SHOW_BKG;
+	SHOW_SPRITES;
+	DISPLAY_ON;
 }
